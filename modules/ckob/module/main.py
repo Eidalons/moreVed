012cf51ae0ee85b8e777170001471cb17ea888fc
Losssaw3 <baylo.04@mail.ru>
2025-05-19 12:21:23 +0300

    fixes

--- modules/ckob/module/main.py
+++ modules/ckob/module/main.py
@@ -63,8 +63,8 @@ ckob = Ckob()
 @app.route('/log-boat-data', methods=['POST'])
 def log_boat_data():
     data = request.get_json()
-    boat_pos = data.get("current_pos")
-    sensors_data = data.get("sensors_data")
+    boat_pos = data.get("current_coords")
+    sensors_data = [data.get("param1") , data.get("param2") 
     print(f"Boat data log: boat_pos: {boat_pos}, sensors_data: {sensors_data}")
     return jsonify({"status": "Boat data successfully logged"}), 200
 
